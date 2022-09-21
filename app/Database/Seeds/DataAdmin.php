c90bb2b333829fcbdfcaa8943e9964b71c1e773b
DanarVirkan <danarvirkan@gmail.com>
2022-09-21 15:17:32 +0700

    added super admin user in seeder

--- app/Database/Seeds/DataAdmin.php
+++ app/Database/Seeds/DataAdmin.php
@@ -14,6 +14,12 @@ class DataAdmin extends Seeder
                 "email" => "test@email.com",
                 "nama" => "Admin 1",
                 "hp" => "08123131",
+            ],
+            [
+                "id_admin" => "adm-666",
+                "email" => "sa@email.com",
+                "nama" => "Super Admin 1",
+                "hp" => "08123131",
             ]
         ];
         $this->db->table('data_admin')->insertBatch($data);
--- app/Database/Seeds/LoginAdmin.php
+++ app/Database/Seeds/LoginAdmin.php
@@ -14,7 +14,13 @@ class LoginAdmin extends Seeder
                 "password" => password_hash("123", PASSWORD_BCRYPT),
                 "akses" => 1,
                 "is_deleted" => false,
-            ]
+            ],
+            [
+                "email" => "sa@email.com",
+                "password" => password_hash("123", PASSWORD_BCRYPT),
+                "akses" => 0,
+                "is_deleted" => false,
+            ],
         ];
         $this->db->table('login_admin')->insertBatch($data);
     }
